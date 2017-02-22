a4822083dd9fd8a0da004f2667fc56023d179cb4
Kevin <knmartinm@gmail.com>
2017-02-21 22:05:09 -0500

    bugs

--- db/migrate/20170221231712_create_users.rb
+++ db/migrate/20170221231712_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name
-      t.integer :facebook_id
+      t.string :facebook_id
       t.string :room_id
       t.boolean :caster
       t.string :profile_picture
