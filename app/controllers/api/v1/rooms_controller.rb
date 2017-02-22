fbf7ecd93ae83fc1c19ad1ce72297add7a055b08
Kevin <knmartinm@gmail.com>
2017-02-21 22:08:06 -0500

    bugs

--- app/controllers/api/v1/rooms_controller.rb
+++ app/controllers/api/v1/rooms_controller.rb
@@ -2,13 +2,13 @@ class Api::V1::RoomsController < Api::ApiV1Controller
 
 	def create
 		@room = Room.create(room_params)
-		render @room
+		render json: @room
 	end
 
 	def edit
 		@room = Room.find_by(token: params[:token])
 		@room.update(status: params[:status])
-		render @room
+		render json: @room
 	end
 
 	private
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,12 +2,12 @@ class Api::V1::UsersController < Api::ApiV1Controller
 
 	def create
 		@user = User.create(user_params)
-		render @user
+		render json: @user
 	end
 
 	def index
 		@users = User.where(room_id: Room.find_by_token(params[:token]).id)
-		render @users
+		render json: @users
 	end
 
 	private
