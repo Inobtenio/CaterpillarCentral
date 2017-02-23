48483330603e6d7367b56ce4cba8fe6130dfcd6c
Kevin Martin <kevin.martin@tektonlabs.com>
2017-02-23 17:34:23 -0500

    m

--- app/controllers/api/v1/rooms_controller.rb
+++ app/controllers/api/v1/rooms_controller.rb
@@ -1,12 +1,13 @@
 class Api::V1::RoomsController < Api::ApiV1Controller
 
 	def create
+		Room.where(caster_id: room_params[:caster_id]).destroy_all
 		@room = Room.create(room_params)
 		render json: @room
 	end
 
 	def edit
-		@room = Room.find_by(caster_id: params[:caster_id], token: params[:token])
+		@room = Room.find_by(caster_id: params[:caster_id])
 		@room.update(status: params[:status])
 		render json: @room
 	end
